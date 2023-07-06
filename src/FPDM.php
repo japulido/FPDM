ee006627fb844b27286b16911f5b24b9355a202c
Juan Antonio Pulido <japulido@gmail.com>
2023-07-06 19:54:26 +0200

    Acceso a pdf_entries

--- src/FPDM.php
+++ src/FPDM.php
@@ -33,7 +33,7 @@ class FPDM
     protected $fdf_source;      //string: full pathname to the input fdf , a form data file
     protected $pdf_output;      //string: full pathname to the resulting filled pdf
 
-    public $pdf_entries;     //array: Holds the content of the pdf file as array
+    protected $pdf_entries;     //array: Holds the content of the pdf file as array
     protected $fdf_content;     //string: holds the content of the fdf file
     protected $fdf_parse_needed;//boolean: false will use $fields data else extract data from fdf content
     protected $value_entries;   //array: a map of values to faliclitate access and changes
@@ -2088,6 +2088,9 @@ class FPDM
         throw new \Exception('FPDF-Merge Error: ' . $msg);
     }
 
-}
+    public function pdfEntries()
+    {
+        return $pdf_entries;
+    }
 
 ?>
